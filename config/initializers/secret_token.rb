7d9a5cbb60866f6ac195f3778b40a03e001e1f70
Blurredbits <mmorris00@mac.com>
2013-07-25 13:03:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlTwitter::Application.config.secret_token = 'a8c621e6c6e3b30220fbe84b21a9b53280de754449c9f7b20cc3a1184449a771a324421304ba3aa8ec7c147151e3e9801a7e90e65540e4581900c086afdf80a1'
